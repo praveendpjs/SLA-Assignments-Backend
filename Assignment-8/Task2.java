cd743ce474dec759f45b67500bceb15464e0c84b
praveendpjs <praveenaishu2012@gmail.com>
2026-01-13 08:30:01 +0530

    Update Task2.java

--- Assignment-8/Task2.java
+++ Assignment-8/Task2.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+
+        System.out.println("Enter elements of matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        int max = matrix1[0][0];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(matrix1[i][j] > max){
+                    max = matrix1[i][j];
+                }
+            }
+        }
+        System.out.println("The greatest number in 2D array is " + max);
     }
 }
